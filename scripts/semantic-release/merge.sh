7dccdef002edcbfa4301fd24a70a859600ab2c9a
Nathan Schott <nschott3@gmail.com>
2019-09-11 11:57:27 -0400

    ci: merge.sh tweak

--- scripts/semantic-release/merge.sh
+++ scripts/semantic-release/merge.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 REPO_URL=$1
-HASH=$(git rev-parse HEAD)
+RELEASE_HASH=$(git rev-parse HEAD)
+
+git remote -v
 
 # Allows fetching and checking out other branches
 git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
@@ -9,8 +11,12 @@ git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 git remote set-url origin ${REPO_URL} > /dev/null 2>&1
 
 # Merge release changes into develop
+git checkout release
+git merge $RELEASE_HASH
+
 git pull --depth=1 origin develop
 git checkout develop
-git merge $HASH
+git merge release
 git push origin develop
-git checkout $HASH
+
+git checkout release
